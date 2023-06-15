baa9d2a9530fcfc2f55e2809b1707dedd42319e2
Carol A. Nikolic <carolnikolic@gmail.com>
2023-06-15 22:00:38 +0200

    merging

--- dadilja-melisa/src/components/Carousel/index.js
+++ dadilja-melisa/src/components/Carousel/index.js
@@ -1,25 +1,23 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, A11y } from 'swiper';
+import { Pagination, A11y } from 'swiper';
 import 'swiper/css';
-import 'swiper/css/navigation';
 import 'swiper/css/pagination';
+import styles from './carousel.module.css';
 
 const Carousel = ({slides, textOne, textTwo}) => {
 
     return (
       <Swiper
-       modules={[Navigation, Pagination, A11y]}
-       navigation
+       className={styles.swiperContainer}
+       modules={[Pagination, A11y]}
        pagination={{ clickable: true }}
        spaceBetween={50}
        slidesPerView={1}
-       onSlideChange={() => console.log('slide change')}
-       onSwiper={(swiper) => console.log(swiper)}
       >
         {slides.map((slide, index) => (
-           <SwiperSlide key={index}>
-             <p>{slide[textOne]}</p>
-             <p>{slide[textTwo]}</p>
+           <SwiperSlide key={index} className={styles.slideContainer}>
+             <p className={styles.firstText}>{slide[textOne]}</p>
+             <p className={styles.secondText}>{slide[textTwo]}</p>
            </SwiperSlide>
         ))}
       </Swiper>
