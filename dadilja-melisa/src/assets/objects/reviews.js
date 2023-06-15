e98b2be6e98291dddc42aa9b5f7bdf9ca0b1b2d9
Carol A. Nikolic <carolnikolic@gmail.com>
2023-06-16 00:31:07 +0200

    chore: correct grammar mistakes and make reviews shorter

--- dadilja-melisa/src/assets/objects/reviews.js
+++ dadilja-melisa/src/assets/objects/reviews.js
@@ -1,6 +1,6 @@
 const reviews = [
     {
-     testimonial: "Melisa is dedicated, cheerful, gentle, and focused. She possesses a great empathy and understanding of children's psychology. She can spend time with children meaningfully and imaginatively, and in a subtle but suggestive way, she clearly sets boundaries and overcomes all delicate educational challenges. Children are always fulfilled and joyful after spending time with her.",
+     testimonial: "Melisa possesses a great empathy and understanding of children's psychology. She spend time with children meaningfully and imaginatively, and in a subtle but suggestive way, she clearly sets boundaries and overcomes all delicate educational challenges. Children are always fulfilled and joyful after spending time with her.",
      writer: "Mina Babić"
     },
 
@@ -15,7 +15,7 @@ const reviews = [
     },
 
     {
-      testimonial: "There is no better and more responsible babysitter in Belgrade than Melisa. She has done a great job with our little one, caring for and teaching her during the pandemic. I truly recommend it!",
+      testimonial: "There isn't a better and more responsible babysitter in Belgrade than Melisa. She has done a great job with our little one, caring for and teaching her during the pandemic. I truly recommend it!",
       writer: "Branislav Nikolić"
     }
 
