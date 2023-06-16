7ccd6c32284a5f25249366dc26358541f48cdbec
Carol A. Nikolic <carolnikolic@gmail.com>
2023-06-16 23:56:31 +0200

    feat: adjust the navbar menu to tablet screen

--- dadilja-melisa/src/components/Navbar/index.js
+++ dadilja-melisa/src/components/Navbar/index.js
@@ -13,16 +13,16 @@ const Navbar = () => {
                     <Image className={styles.logo} src="./svgs/logo/logo-horizontal.svg" width={100} height={80} alt="Dadilja Melisa logo"/>
                 </div>
 
-                <div className={styles.listContainer}>
+                <div className={`${styles.listContainer} ${styles.listContainerRelative}`}>
                     <Menu menuTitle="Menu" toggle={{menu, setMenu}}/>
-                    {menu &&
-                    <ul className={styles.list}>
+                    
+                    <ul className={`${styles.list} ${menu && styles.active}`}>
                         <li><Link className={styles.listItem} href="#home">Home</Link></li>
                         <li><Link className={styles.listItem} href="#about">About</Link></li>
                         <li><Link className={styles.listItem} href="#prices">Prices</Link></li>
                         <li><Link className={styles.listItem} href="#contact">Contact</Link></li> 
                     </ul>
-                    }
+                    
                 </div>
 
                 <div className={styles.listContainer}>
