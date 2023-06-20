a191b2682c1ccd1909aea93d53fe34b529914341
Carol A. Nikolic <carolnikolic@gmail.com>
2023-06-20 15:05:28 +0200

    feat: adjust footer for tablet screen

--- dadilja-melisa/src/components/Footer/index.js
+++ dadilja-melisa/src/components/Footer/index.js
@@ -12,25 +12,31 @@ const Footer = () => {
             <Image className={styles.logo} src="/svgs/logo/logo-vertical.svg" width={149} height={103} alt="Dadilja Melisa logo in vertical"/>
 
             <div className={styles.footerText}>
-                <h1 className={styles.title}>About</h1>
+                <div className={styles.block}>
+                    <h1 className={styles.title}>About</h1>
                     <ul className={styles.linkList}>
                         <li><Link className={styles.link} href="/">Home</Link></li>
                         <li><Link className={styles.link} href="/">About</Link></li>
                         <li><Link className={styles.link} href="/">Reviews</Link></li>
                     </ul>
-                
-                <h1 className={styles.title}>Services</h1>
+                </div>
+            
+                <div className={styles.block}>
+                    <h1 className={styles.title}>Services</h1>
                     <ul className={styles.linkList}>
                         <li><Link className={styles.link} href="/">Prices</Link></li>
                         <li><Link className={styles.link} href="/">Contact</Link></li>
                     </ul>
-                    
-                <h1 className={styles.title}>Contact</h1>
+                </div>
+
+                <div className={styles.block}>
+                    <h1 className={styles.title}>Contact</h1>
                     <ul className={styles.linkList}>
                         <li className={styles.contactInfo}><Image src="/svgs/icons/map-pin.svg" width={20} height={20} alt="map pin"/>Narodnog Fronta 77lj, MML</li>
                         <li className={styles.contactInfo}><Image src="/svgs/icons/mail.svg" width={20} height={20} alt="mail"/><a className={styles.link} href={mailtoUrl}>dadiljamelisa@gmail.com</a></li>
                         <li className={styles.contactInfo}><Image src="/svgs/icons/phone.svg" width={20} height={20} alt="phone"/><a className={styles.link} href={telNumber}>064/0808-316</a></li>
                     </ul>
+                </div>       
             </div>
             
             <div className={styles.copyright}>
