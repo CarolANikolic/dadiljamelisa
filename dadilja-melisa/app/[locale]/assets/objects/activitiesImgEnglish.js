0670a27d95a525180cd5f80d13e1867c7d5127de
Carol A. Nikolic <carolnikolic@gmail.com>
2023-07-26 18:29:48 +0200

    feat: include images of activities in serbian

--- dadilja-melisa/app/[locale]/assets/objects/activitiesImgEnglish.js
+++ dadilja-melisa/app/[locale]/assets/objects/activitiesImgEnglish.js
@@ -1,27 +1,27 @@
 const activitiesImg = [
     {
-        source: "/svgs/elements/activity-one.jpg",
+        source: "/activities-imgs-en/creativity-en.jpg",
         alt: "creativity and imagination",
         heading: "Elevate Your Child's Learning Experience with Our Enriching Workshops",
         paragraph: "My workshops are designed to foster your child's creativity and imagination while honing their fine motor skills through engaging activities with unstructured and semi-structured materials. Your child will be encouraged to explore their interests and develop their skills in a supportive and nurturing environment.",
         color: "green"
     },
     {
-        source: "/svgs/elements/activity-two.jpg",
+        source: "/activities-imgs-en/interactivity-en.jpg",
         alt: "interactivity and growth",
         heading: "Exciting Educational Programs for Your Child's Growth",
         paragraph: "I offer a diverse educational program tailored to stimulate your child's interests in various fields such as math, speech, environmental awareness, music, physics, and art. Through hands-on projects and interactive learning, your child will be inspired to learn and develop new skills, building a solid foundation for their future success.",
         color: "yellow"
     },
     {
-        source: "/svgs/elements/activity-three.jpg",
+        source: "/activities-imgs-en/playfulness-en.jpg",
         alt: "playfullness and socialization",
         heading: "Fun-filled Sports Activities for Your Child's Physical Development",
         paragraph: "We believe that physical activities are crucial for your child's overall development. Our sports activities, including relay games, competitive games, polygons, story polygons, and dance activities, are designed to promote physical health, teamwork, and social skills in a fun and enjoyable way.",
         color: "beige"
     },
     {
-        source: "/svgs/elements/activity-four.jpg",
+        source: "/activities-imgs-en/exploration-en.jpg",
         alt: "exploration and discovery",
         heading: "Excursions and Field Trips to Expand Your Child's Horizons",
         paragraph: "I offer exciting opportunities for your child to explore the world through organized excursions and field trips. Your child will have the chance to visit local communities, museums, theaters, and other enriching destinations, providing them with valuable real-world experiences and broadening their horizons.",
