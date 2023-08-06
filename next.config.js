862b1dd1f47e373e9e1017cc4a96aa1459459c3f
Eduardo <eduardo.arakaki@hotmail.com>
2023-08-06 12:51:31 +0900

    wip: create a redirect for /sr

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	async redirects() {
-		return [
-			{
-				source: "/",
-				destination: "/sr",
-				permanent: true,
-			},
-		];
-	},
+	distDir: "public",
 };
 
 module.exports = nextConfig;
